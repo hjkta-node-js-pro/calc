fe544c06a23e7410b8e0422a8dfc85526db7117d
hjkta <fux.aleksander@yandex.ru>
2022-07-26 01:18:56 +0300

    add events support

--- index.mjs
+++ index.mjs
@@ -1,7 +1,18 @@
 import process from "process";
+import EventEmitter from "events";
 
 import * as calc from "./es/index.mjs";
 
 const [operation, operand1, operand2] = process.argv.slice(2);
 
-console.log(calc[operation](Number(operand1), Number(operand2)));
+const evt = new EventEmitter();
+
+for (let func in calc) {
+	evt.once(func, ([operand1, operand2]) =>
+		evt.emit("result", calc[operation](Number(operand1), Number(operand2)))
+	);
+}
+
+evt.once("result", (result) => console.log(result));
+
+evt.emit(operation, [operand1, operand2]);
